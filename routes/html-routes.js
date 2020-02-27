4f52ddb9365e5d0c138204a4d69e2ae6c10bdd03
Ryan Larsen <larsenr12@gmail.com>
2020-02-27 17:53:04 -0500

    Updated html routes for voting path

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -6,7 +6,7 @@ const isAuthenticated = require('../config/middleware/isAuthenticated')
 
 module.exports = function (app) {
   app.get('/', function (req, res) {
-    // If the user already has an account send them to the members page
+    // If the user already has an account send them to the voting page
     if (req.user) {
       res.redirect('/vote')
     }
@@ -14,16 +14,16 @@ module.exports = function (app) {
   })
 
   app.get('/login', function (req, res) {
-    // If the user already has an account send them to the members page
+    // If the user already has an account send them to the voting page
     if (req.user) {
-      res.redirect('/members')
+      res.redirect('/vote')
     }
     res.sendFile(path.join(__dirname, '../public/login.html'))
   })
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get('/members', isAuthenticated, function (req, res) {
-    res.sendFile(path.join(__dirname, '../public/members.html'))
+  app.get('/vote', isAuthenticated, function (req, res) {
+    res.sendFile(path.join(__dirname, '../public/vote.html'))
   })
 }
