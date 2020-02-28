7939ac42b182b589387604570826db74ea91724b
Lucas Gonzalez <lucasbertaudgonzalez@gmail.com>
2020-02-28 16:00:13 -0500

    updated the '/vote' route in the html-routes.js

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -24,6 +24,6 @@ module.exports = function (app) {
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get('/vote', isAuthenticated, function (req, res) {
-    res.sendFile(path.join(__dirname, '../public/vote.html'))
+    res.sendFile(path.join(__dirname, '../public/assets/html/vote.html'))
   })
 }
