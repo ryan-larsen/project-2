4e22428f556de64fd9ad6e70df3cc6a7d38397cb
Lucas-bit <lucasbertaudgonzalez@gmail.com>
2020-03-06 18:18:55 -0500

    merged khalid work

--- models/index.js
+++ models/index.js
@@ -1,5 +1,4 @@
 'use strict'
-
 const fs = require('fs')
 const path = require('path')
 const Sequelize = require('sequelize')
@@ -7,30 +6,33 @@ const basename = path.basename(module.filename)
 const env = process.env.NODE_ENV || 'development'
 const config = require(__dirname + '/../config/config.json')[env]
 const db = {}
-
+console.log(config)
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable])
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config)
 }
-
+console.log('Past If')
 fs
   .readdirSync(__dirname)
   .filter(function (file) {
+    if (file === 'Vote.js') {
+      return false
+    }
+    console.log(file)
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js')
   })
   .forEach(function (file) {
     const model = sequelize.import(path.join(__dirname, file))
     db[model.name] = model
   })
-
+console.log('After fs')
 Object.keys(db).forEach(function (modelName) {
   if (db[modelName].associate) {
     db[modelName].associate(db)
   }
 })
-
+console.log(sequelize)
 db.sequelize = sequelize
 db.Sequelize = Sequelize
-
 module.exports = db
--- server.js
+++ server.js
@@ -1,15 +1,38 @@
 // Requiring necessary npm packages
 const express = require('express')
 const session = require('express-session')
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport')
 // Requiring passport as we've configured it
-const passport = require('./config/passport')
+console.log('STAGE 0')
 
+// const passport = require('./config/passport')
+// DB Config
+require('./config/db');
+const app = express();
+const poll = require('./routes/poll');
+// Set public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+console.log('STAGE 1')
+
+// Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Enable CORS
+app.use(cors());
+app.use('/poll', poll);
+
+console.log('STAGE 2')
 // Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 3000
 const db = require('./models')
 
 // Create express app and configure middleware needed for authentication
-const app = express()
+console.log('STAGE 3')
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(express.static('public'))
@@ -18,14 +41,16 @@ app.use(express.static('public'))
 app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized: true }))
 app.use(passport.initialize())
 app.use(passport.session())
+console.log('STAGE 4')
 
 // Requiring routes
 require('./routes/html-routes.js')(app)
 require('./routes/api-routes.js')(app)
+console.log('STAGE 5')
 
 // Syncing database and logging message
 db.sequelize.sync().then(function () {
   app.listen(PORT, function () {
     console.log('App listening on PORT ', PORT)
   })
-})
+});
