9235e9a76e8f67b412ec8c261e5d0b228081dc3b
Ryan <larsenr12@gmail.com>
2020-03-07 00:43:56 -0500

    new pull to master

--- models/Vote.js
+++ models/Vote.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 const VoteSchema = new Schema({
   os: {
     type: String,
@@ -9,7 +9,7 @@ const VoteSchema = new Schema({
     type: String,
     required: true
   }
-});
+})
 // Create collection and add schema
-const Vote = mongoose.model('Vote', VoteSchema);
-module.exports = Vote;
+const Vote = mongoose.model('Vote', VoteSchema)
+module.exports = Vote
