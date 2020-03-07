5f4be8924f08b845e56b9b58c2c30428b657fe1f
Ryan <larsenr12@gmail.com>
2020-03-07 08:53:52 -0500

    fixed signup route

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -28,7 +28,7 @@ module.exports = function (app) {
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the login page
-  app.get('/vote',  function (req, res) {
+  app.get('/vote', function (req, res) {
     res.sendFile(path.join(__dirname, '../public/assets/html/vote.html'))
   })
 }
